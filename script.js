f6b340b2fc60d080d0e49c6e5e8966ba669c3555
Jork Cedric <jcp093001@gmail.com>
2023-02-02 17:24:11 +0800

    add local storage function

--- script.js
+++ script.js
@@ -6,6 +6,38 @@ const counterNum = document.querySelector('.counter');
 
 const counterNav = document.querySelector('.counter-navigation');
 
+// let value = 0;
+let value = localStorage.getItem('value')
+
+function resetVal() {
+    value = 0;
+    counterNum.textContent = value;
+    localStorage.setItem('value', value);
+
+}
+
+incrementBtn.addEventListener('click', () => {
+    value++;
+    counterNum.textContent = value;
+    localStorage.setItem('value', value);
+
+})
+
+resetBtn.addEventListener('click', resetVal)
+
+decrementBtn.addEventListener('click', () => {
+    value--;
+    if (value <= 0) {
+        resetVal();
+    }
+    counterNum.textContent = value;
+    localStorage.setItem('value', value);
+
+})
+
 startBtn.addEventListener('click', () => {
     counterNav.classList.toggle('show');
 })
+
+// const value = localStorage.getItem('value');
+counterNum.textContent = value;
